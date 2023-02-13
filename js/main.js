05c4f7610face378785d06f2f7651d24eaff9f5b
FloppaSlave <korstim18@gmail.com>
2023-02-13 10:18:40 +0700

    Выполнил 3 практическую (изменил логику рендера под себя)

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 let app = new Vue({
     el: '#app',
     data: {
-        productName: "Socks",
-        altText: "A pair of socks",
+        product: "Socks",
         image: "./assets/vmSocks-green-onWhite.jpg",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks."
+        altText: "A pair of socks",
+        inStock: false, 
+        inventory: 10,
+        OnSale: true,
+
     }
  })
+ 
  
 //  Экземпляр Vue —  Этот объект содержит различные свойства и методы, которые дают экземпляру Vue возможность хранить данные и выполнять какие-то действия.
 
