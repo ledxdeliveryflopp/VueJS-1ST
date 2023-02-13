b2efef45eb6f0064e9992c5b34fa5413785a695a
FloppaSlave <korstim18@gmail.com>
2023-02-13 11:36:43 +0700

    Выполнил 5 практическую работу (были добавлены проверки и сокращены события )

--- js/main.js
+++ js/main.js
@@ -5,14 +5,41 @@ let app = new Vue({
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         inStock: false, 
-        inventory: 10,
+        inventory: 12,
         OnSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [{variantId: 2234, variantColor: 'green'}, {variantId: 2235, variantColor: 'blue'}],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+        variants: [{variantId: 2234, variantColor: 'green', variantImage: "./assets/vmSocks-green-onWhite.jpg"}, 
+                  {variantId: 2235, variantColor: 'blue', variantImage: "./assets/vmSocks-blue-onWhite.jpg"}],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 1,
+
+             addToCart() {
+                if(this.inventory <= 0)
+                    alert("Товар закончился")
+                else{
+                    this.cart += 1
+                    this.inventory -= 1
+                 }
+
+             } ,
+
+            deleteFromCart() {
+                if(this.cart <= 0)
+                    alert("Корзина пуста")
+                else{
+                this.cart -= 1
+                this.inventory += 1
+                }
+            },
+
+            updateProduct(variantImage) {
+                this.image = variantImage
+            }
     }
  })
  
  
 //  Экземпляр Vue —  Этот объект содержит различные свойства и методы, которые дают экземпляру Vue возможность хранить данные и выполнять какие-то действия.
 
+
+ 
